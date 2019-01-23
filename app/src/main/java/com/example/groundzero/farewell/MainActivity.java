85e4f642d9a2e9996193f218dff878c718c42b1f
james_njoro <jnjoroge@gmail.com>
2019-01-23 11:23:12 +0300

    app drawer added

--- app/src/main/java/com/example/groundzero/farewell/MainActivity.java
+++ app/src/main/java/com/example/groundzero/farewell/MainActivity.java
@@ -1,18 +1,24 @@
 package com.example.groundzero.farewell;
 
 import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.view.ViewGroup;
-import android.support.v7.widget.Toolbar;
 import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
@@ -25,7 +31,8 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
     private FirebaseAuth mAuth;
     private Toolbar mtoolbar;
     FirebaseDatabase firebaseDatabase;
@@ -35,75 +42,122 @@ public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     InternetCheck check;
 
-
-
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         mAuth = FirebaseAuth.getInstance();
         firebaseDatabase = FirebaseDatabase.getInstance();
         databaseReference = firebaseDatabase.getReference("EDMT_FIREBASE");
         FirebaseUser currentUser = mAuth.getCurrentUser();
-        mtoolbar = (Toolbar)findViewById(R.id.mainActionBar);
-        setSupportActionBar(mtoolbar);
-        getSupportActionBar().setTitle("Farewell");
         recyclerView= (RecyclerView)findViewById(R.id.recycler);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         check = new InternetCheck(this);
 
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
         if(check.isConnected()){
-            Toast.makeText(MainActivity.this, "Please connect to the internet",
-                    Toast.LENGTH_SHORT).show();
-        }else{
             Toast.makeText(MainActivity.this, "connection Successful",
+                    Toast.LENGTH_SHORT).show();
+
+        }else{
+            Toast.makeText(MainActivity.this, "Please connect to the internet",
                     Toast.LENGTH_SHORT).show();
         }
 
 
         if(currentUser==null){
-           startScreen();
+            startScreen();
         }else{
             displaC();
             adapter.notifyDataSetChanged();
 
         }
 
-        }
+    }
 
-        public void startScreen(){
-            Intent i = new Intent(MainActivity.this,LoginActivity.class);
-            startActivity(i);
-            finish();
+    public void startScreen(){
+        Intent i = new Intent(MainActivity.this,LoginActivity.class);
+        startActivity(i);
+        finish();
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
         }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        super.onCreateOptionsMenu(menu);
-        getMenuInflater().inflate(R.menu.main_menu,menu);
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        super.onOptionsItemSelected(item);
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id =item.getItemId();
         if(item.getItemId()==R.id.logoutButton){
             FirebaseAuth.getInstance().signOut();
             startScreen();
         }
 
-        return true;
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.logoutButton) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
     private void displaC() {
-         options =
+        options =
                 new FirebaseRecyclerOptions.Builder<postI>()
-                .setQuery(databaseReference,postI.class)
-                .build();
-         adapter =
+                        .setQuery(databaseReference,postI.class)
+                        .build();
+        adapter =
                 new FirebaseRecyclerAdapter<postI, MyAdapter>(options) {
                     @Override
                     protected void onBindViewHolder(@NonNull MyAdapter holder, int position, @NonNull postI model) {
@@ -119,8 +173,8 @@ public class MainActivity extends AppCompatActivity {
                         return new MyAdapter(itemview);
                     }
                 };
-         adapter.startListening();
-         recyclerView.setAdapter(adapter);
+        adapter.startListening();
+        recyclerView.setAdapter(adapter);
     }
 
     public void add(View view){
@@ -165,3 +219,5 @@ public class MainActivity extends AppCompatActivity {
 
 
 }
+
+
